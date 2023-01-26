28d13ed3d34b1ad060d4d1f9191983d97fee5422
morux2 <k-morimoto@keio.jp>
2023-01-27 00:41:25 +0900

    add sticky header

--- app/src/main/java/com/example/stickytabheadersample/MainActivity.kt
+++ app/src/main/java/com/example/stickytabheadersample/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.stickytabheadersample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
 import androidx.compose.material.Text
@@ -14,9 +16,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.stickytabheadersample.ui.theme.StickyTabHeaderSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,24 +31,34 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SampleScreen() {
     var tabSelected by rememberSaveable { mutableStateOf(Screen.A) }
-    Column {
-        TabRow(
-            selectedTabIndex = tabSelected.ordinal
-        ) {
-            Screen.values().map { it.name }.forEachIndexed { index, title ->
-                Tab(
-                    text = { Text(text = title) },
-                    selected = tabSelected.ordinal == index,
-                    onClick = { tabSelected = Screen.values()[index] }
-                )
+    LazyColumn {
+        item {
+            Text(
+                modifier = Modifier.padding(vertical = 100.dp).fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                text = "collapsed!"
+            )
+        }
+        stickyHeader {
+            TabRow(
+                selectedTabIndex = tabSelected.ordinal
+            ) {
+                Screen.values().map { it.name }.forEachIndexed { index, title ->
+                    Tab(
+                        text = { Text(text = title) },
+                        selected = tabSelected.ordinal == index,
+                        onClick = { tabSelected = Screen.values()[index] }
+                    )
+                }
             }
         }
         when (tabSelected) {
-            Screen.A -> AScreen()
-            Screen.B -> BScreen()
+            Screen.A -> aScreen(this@LazyColumn)
+            Screen.B -> bScreen(this@LazyColumn)
         }
     }
 }
@@ -62,26 +75,34 @@ fun DefaultPreview() {
     }
 }
 
-@Composable
-fun AScreen() {
-    LazyColumn(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        items(10) {
-            Text("A $it")
+fun aScreen(
+    lazyListScope: LazyListScope
+) {
+    with(lazyListScope) {
+        items(30) {
+            Text(
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                text = "A $it"
+            )
         }
     }
 }
 
-@Composable
-fun BScreen() {
-    LazyColumn(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        items(10) {
-            Text("B $it")
+fun bScreen(
+    lazyListScope: LazyListScope
+) {
+    with(lazyListScope) {
+        items(30) {
+            Text(
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                text = "B $it"
+            )
         }
     }
 }
